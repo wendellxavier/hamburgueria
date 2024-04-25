2aee25191594579fe8094646e15e6d903b39ac08
wendellxavier <wendell_xw20@hotmail.com>
2024-04-25 11:03:21 -0300

    teste

---
+++ burguer/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from burguer.models import Produto
+from django.http import HttpResponse
+
+def home(request):
+    produtos = Produto.objects.all()
+    context = {
+        'produtos': produtos
+    }
+    return render(request, 'burguer/home.html', context)
+
+def detalhe_produto(request, produto_id):
+    produto = Produto.objects.get(pk=produto_id)
+    context = {
+        'produto': produto
+    }
+    return render(request, 'burguer/produto.html', context)
